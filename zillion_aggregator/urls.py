f1856a41a89cca08a52369dd5711bb25f59d89f9
NiaX84 <spochybo@gmail.com>
2019-10-27 18:56:34 +0100

    adding address reader

--- zillion_aggregator/urls.py
+++ zillion_aggregator/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'zillion_aggregator'
 urlpatterns = [
+    path('', views.read_addresses, name='read_addresses'),
     path('', views.read_json, name='read_json'),
 ]
