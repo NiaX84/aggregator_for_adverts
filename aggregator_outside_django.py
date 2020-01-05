6def1f004da31605b19b912ab1030b86adc87d09
NiaX84 <spochybo@gmail.com>
2020-01-05 22:02:32 +0100

    extending to all records

--- aggregator_outside_django.py
+++ aggregator_outside_django.py
@@ -16,8 +16,14 @@ if __name__ == '__main__':
     only_descriptions = aggregated_records[:, 1]
     tf_idf_vectorizer = TfidfVectorizer()
     tf_idf_matrix = tf_idf_vectorizer.fit_transform(only_descriptions)
-    description_similarity = cosine_similarity(tf_idf_matrix[0:1], tf_idf_matrix)
-    record_ids = aggregated_records[np.where(description_similarity > 0.5)[1], 0]
-
-    aggregated_records_df.loc[aggregated_records_df.record_id.isin(record_ids), 'similarity_id'] = 1
+    selected_records = []
+    similarity_id = 0
+    for i in range(0, tf_idf_matrix.shape[0]):
+        if i+1 in selected_records:
+            continue
+        description_similarity = cosine_similarity(tf_idf_matrix[i:i+1], tf_idf_matrix)
+        record_ids = aggregated_records[np.where(description_similarity > 0.5)[1], 0]
+        similarity_id += 1
+        aggregated_records_df.loc[aggregated_records_df.record_id.isin(record_ids), 'similarity_id'] = similarity_id
+        selected_records.extend(record_ids)
     print(aggregated_records_df)
